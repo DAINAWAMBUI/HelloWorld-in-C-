12af47632e22377fb2e34ea6c0480591be6eada4
Daina Muiruri <61407935+DAINAWAMBUI@users.noreply.github.com>
2024-05-14 09:21:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+namespace HelloWorld
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I'm excited to start my journey in C#");
+            Console.WriteLine("looking forward to learning more and more about C#");
+        }
+    }
+}
+
